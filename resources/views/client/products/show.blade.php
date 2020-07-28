ff68695608fefcb22247dc4f4bef13197cbebb81
Long Do <me@thinky.cz>
2020-07-28 13:59:04 +0200

    Product page completed

--- resources/views/client/products/show.blade.php
+++ resources/views/client/products/show.blade.php
@@ -1,41 +1,96 @@
 @extends('client.layout')
 
 @section('content')
-    <div class="flex w-full">
-        <div>
-            <img src="https://static.kosik.cz/images/thumbs/af/860x800x1_af66528708c963c2900c06d4a7f3afe5.jpg" class="w-96 h-96">
+    <div class="space-y-8">
+
+        <div class="flex w-full space-x-6 p-6 bg-white rounded border border-gray-200">
+            <div class="w-1/3">
+                <img src="{{ $product->thumbnail }}" class="w-full">
+            </div>
+
+            <div class="flex-1 space-y-6">
+                <h2 class="text-2xl text-gray-700 font-semibold">
+                    {{ $product->name }}
+                </h2>
+
+                <p class="text-gray-700 text-sm">
+                    {{ $product->description }}
+                </p>
+
+                <div class="flex justify-between items-center">
+                    <div class="w-1/2">
+                        <p class="text-2xl font-semibold text-teal-700">{{ $product->formatted_price }}</p>
+                        <p class="text-xs font-semibold text-gray-500">{{ $product->formatted_price_excl_vat }} bez
+                            DPH</p>
+                    </div>
+
+                    <div class="w-1/2">
+                        <livewire:add-to-basket :product="$product"></livewire:add-to-basket>
+                    </div>
+                </div>
+
+                <hr>
+
+                <ul>
+                    <li class="py-2 flex justify-between text-xs font-semibold">
+                        <span class="text-cool-gray-500">Min. obj.</span>
+                        <span class="text-gray-700">{{ $product->moq }}</span>
+                    </li>
+                    <li class="py-2 flex justify-between text-xs font-semibold">
+                        <span class="text-cool-gray-500">Katalog</span>
+                        <span class="text-gray-700">{{ $product->catalog }}</span>
+                    </li>
+                    <li class="py-2 flex justify-between text-xs font-semibold">
+                        <span class="text-cool-gray-500">Čárový kód</span>
+                        <span class="text-gray-700">{{ $product->barcode }}</span>
+                    </li>
+                    <li class="py-2 flex justify-between text-xs font-semibold">
+                        <span class="text-cool-gray-500">Skladem</span>
+                        <span class="text-gray-700">{{ $product->public_stock_quantity }} {{ $product->unit->abbr }}</span>
+                    </li>
+                </ul>
+            </div>
         </div>
 
-        <div class="flex-1 space-y-4">
-            <h2 class="text-2xl text-gray-700 font-semibold">
-                {{ $product->name }}
-            </h2>
-            <p class="text-2xl text-red-500">19.90 Kc</p>
-            <p class=" text-gray-700 text-sm">
-                Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Proin eget tortor risus.
-            </p>
-
-            <div class="flex space-x-2">
-                <div x-data="{ count: 1 }" class="bg-gray-300 flex items-center rounded">
-                    <button class="p-4 focus:outline-none" x-on:click="count <= 1 ? count = 1 : count -= 1"> - </button>
-                    <input type="text" class="h-8 w-12 focus:outline-none rounded text-center" x-bind:value="count">
-                    <button class="p-4 focus:outline-none" x-on:click="count += 1"> + </button>
+        <div class="p-6 bg-white rounded border border-gray-200 space-y-6">
+            <div>
+                <div class="sm:hidden">
+                    <select aria-label="Selected tab" class="form-select block w-full">
+                        <option selected>Product Details</option>
+                        {{--                        <option>Properties</option>--}}
+                        {{--                        <option>Ratings</option>--}}
+                        {{--                        <option>Comments</option>--}}
+                    </select>
                 </div>
-
-                <div>
-                    <button class="focus:outline-none h-full px-4 w-full bg-teal-500 hover:bg-teal-400 text-white text-md font-semibold flex items-center justify-center rounded">
-                        <x-icons.shopping-bag class="w-4 h-4 mr-2"></x-icons.shopping-bag>
-                        Add to cart
-                    </button>
-                </div>
-
-                <div x-data="{ liked: false }" class="bg-gray-300 rounded p-4">
-                    <button x-on:click="liked = !liked" class="focus:outline-none">
-                        <x-icons.heart class="w-6 h-6" x-bind:class="{'text-red-500': liked}"></x-icons.heart>
-                    </button>
+                <div class="hidden sm:block">
+                    <div class="border-b border-gray-200">
+                        <nav class="-mb-px flex">
+                            <a href="#"
+                               class="w-1/4 py-4 px-1 text-center border-b-2 border-teal-500 font-medium text-sm leading-5 text-teal-600 focus:outline-none focus:text-teal-800 focus:border-teal-700"
+                               aria-current="page">
+                                Product Details
+                            </a>
+                            {{--                            <a href="#"--}}
+                            {{--                               class="w-1/4 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">--}}
+                            {{--                                Properties--}}
+                            {{--                            </a>--}}
+                            {{--                            <a href="#"--}}
+                            {{--                               class="w-1/4 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">--}}
+                            {{--                                Ratings--}}
+                            {{--                            </a>--}}
+                            {{--                            <a href="#"--}}
+                            {{--                               class="w-1/4 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">--}}
+                            {{--                                Comments--}}
+                            {{--                            </a>--}}
+                        </nav>
+                    </div>
                 </div>
             </div>
 
+            <div>
+                {!! $product->details !!}
+            </div>
         </div>
+
     </div>
 @endsection
--- resources/views/livewire/product-card.blade.php
+++ resources/views/livewire/product-card.blade.php
@@ -1,9 +1,15 @@
 <li class="col-span-1 flex flex-col bg-white rounded-lg shadow">
     <div class="flex-1 flex flex-col p-4">
-        <img class="w-full h-40 flex-shrink-0 mx-auto object-contain" src="{{ $product->thumbnail }}" alt="{{ $product->name }}" />
-        <a href="{{ route('products.show', $product) }}" class="mt-6 text-gray-900 text-sm leading-5 font-medium hover:underline">
+        <a href="{{ route('products.show', $product) }}">
+            <img class="w-full h-40 flex-shrink-0 mx-auto object-contain" src="{{ $product->thumbnail }}"
+                 alt="{{ $product->name }}"/>
+        </a>
+
+        <a href="{{ route('products.show', $product) }}"
+           class="mt-6 text-gray-900 text-sm leading-5 font-medium hover:underline">
             {{ $product->name }}
         </a>
+
         <dl class="mt-3 flex-grow flex flex-col justify-between">
             <dt class="sr-only">Price</dt>
             <dd class="text-teal-500 text-xl font-semibold leading-5">{{ $product->formatted_price }}</dd>
