935bc06745ef8db2c2a21c86080218cd4118d5cd
Long Do <me@thinky.cz>
2020-08-06 20:58:23 +0200

    Price level fix

--- app/Models/PriceLevel.php
+++ app/Models/PriceLevel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class PriceLevel extends Model
 {
@@ -11,5 +12,10 @@ class PriceLevel extends Model
     public function prices()
     {
         return $this->hasMany(Price::class);
+    }
+
+    public function getImportCodeAttribute()
+    {
+        return Str::slug($this->name, '_');
     }
 }
