af9a06c0589f7d0b88bbaa7632faa3552a791dd1
Long Do <me@thinky.cz>
2020-08-07 23:16:06 +0200

    Tag manager installed

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -31,9 +31,26 @@
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.3.5/dist/alpine.min.js" defer></script>
 
         @yield('styles')
+
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PRGNZDM');
+        </script>
+        <!-- End Google Tag Manager -->
     </head>
 
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRGNZDM"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         @yield('body')
 
         @include('layouts.partials.notifications')
