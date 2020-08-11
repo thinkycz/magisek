515f09cb2e1226a0caed8a956fd3b5d28bf75dc6
Long Do <me@thinky.cz>
2020-08-11 20:53:33 +0200

    Filepond allow multiple

--- resources/views/admin/products/partials/photos.blade.php
+++ resources/views/admin/products/partials/photos.blade.php
@@ -1,6 +1,6 @@
 <x-accordion :title="__('products.photos')">
     <div class="p-4">
-        <x-filepond name="filepond" :route="route('acp.products.upload-photo', $product)"></x-filepond>
+        <x-filepond name="filepond" :route="route('acp.products.upload-photo', $product)" multiple="true"></x-filepond>
 
         <livewire:product-photos :product="$product"></livewire:product-photos>
     </div>
