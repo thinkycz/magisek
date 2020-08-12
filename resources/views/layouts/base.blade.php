d17023823c3a3d4819d991e85c86f33f3e321977
Long Do <me@thinky.cz>
2020-08-12 15:11:02 +0200

    Facebook chat widget

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -55,6 +55,33 @@
         </noscript>
         <!-- End Google Tag Manager (noscript) -->
 
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    xfbml            : true,
+                    version          : 'v8.0'
+                });
+            };
+
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/cs_CZ/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+        <!-- Your Chat Plugin code -->
+        <div class="fb-customerchat"
+             attribution=setup_tool
+             page_id="112907143829486"
+             theme_color="#20cef5"
+             logged_in_greeting="Jak ti může Magísek pomoci?"
+             logged_out_greeting="Jak ti může Magísek pomoci?">
+        </div>
+
         @yield('body')
 
         @include('layouts.partials.notifications')
