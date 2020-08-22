856c4f03d9d97c714de2abbf94d247ee87a64aa3
Long Do <me@thinky.cz>
2020-08-22 17:38:02 +0200

    Messages removed from fb widget

--- resources/views/components/facebook-widget.blade.php
+++ resources/views/components/facebook-widget.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full overflow-auto mt-6">
-    <div class="fb-page" data-href="https://www.facebook.com/magisekcz/" data-tabs="timeline, messages" data-width="256"
+    <div class="fb-page" data-href="https://www.facebook.com/magisekcz/" data-tabs="timeline" data-width="256"
          data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
          data-show-facepile="false">
         <blockquote cite="https://www.facebook.com/magisekcz/" class="fb-xfbml-parse-ignore"><a
