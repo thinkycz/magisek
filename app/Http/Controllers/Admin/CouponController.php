8c382f9ec5385f6da7fdfd5d291aa235a81e6da7
Long Do <me@thinky.cz>
2020-08-23 13:39:04 +0200

    Coupon validation fixed

--- app/Http/Controllers/Admin/CouponController.php
+++ app/Http/Controllers/Admin/CouponController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Coupon;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class CouponController extends Controller
 {
@@ -52,18 +53,27 @@ class CouponController extends Controller
 
     protected function data(Request $request)
     {
-        return $request->validate([
-            'code'            => 'required',
+        $data = $request->validate([
+            'code'            => ['required', Rule::unique('coupons')->ignore(optional($request->coupon)->id)],
             'description'     => 'required',
             'value'           => 'required',
             'times_used'      => 'sometimes|nullable',
-            'max_usage'       => 'sometimes|nullable',
-            'valid_from'      => 'required',
-            'valid_to'        => 'required',
+            'max_usage'       => 'numeric|min:0',
+            'valid_from'      => 'required|date|before:2038-01-01|before_or_equal:valid_to',
+            'valid_to'        => 'required|date|before:2038-01-01|after_or_equal:valid_from',
             'enabled'         => 'boolean',
             'once_per_user'   => 'boolean',
             'can_be_combined' => 'boolean',
             'is_percentage'   => 'boolean',
         ]);
+
+
+        if ($data['is_percentage']) {
+            $request->validate([
+                'value' => 'numeric|min:0|max:100'
+            ]);
+        }
+
+        return $data;
     }
 }
--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Coupon extends Model
 {
     protected $guarded = [];
 
     protected $dates = ['valid_from', 'valid_to'];
+
+    public function setCodeAttribute($value)
+    {
+        $this->attributes['code'] = Str::of($value)->slug('_')->upper();
+    }
 }
