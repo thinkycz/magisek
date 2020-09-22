17439e6dba3ef519e642ae91ab3402b57da54ac9
Long Do <me@thinky.cz>
2020-09-22 18:54:38 +0200

    Added fb, ig to sidebar

--- resources/views/client/partials/sidebar.blade.php
+++ resources/views/client/partials/sidebar.blade.php
@@ -1,3 +1,14 @@
 <x-sidebar-categories></x-sidebar-categories>
 
+<div class="mt-8 px-2 flex flex-col space-y-4">
+    <a href="https://www.facebook.com/magisekcz" target="_blank">
+        <span class="sr-only">Facebook</span>
+        <img src="{{ asset('img/follow_fb.png') }}" alt="Facebook">
+    </a>
+    <a href="https://www.instagram.com/magisek_cz" target="_blank">
+        <span class="sr-only">Instagram</span>
+        <img src="{{ asset('img/follow_ig.png') }}" alt="Instagram">
+    </a>
+</div>
+
 {{--<x-facebook-widget></x-facebook-widget>--}}
