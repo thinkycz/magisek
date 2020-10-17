104b8631ee15031be577048de20fe63b67cd10c8
Long Do <me@thinky.cz>
2020-10-17 19:22:55 +0200

    Heureka xml feed generator part 4

--- app/Tools/HeurekaItem.php
+++ app/Tools/HeurekaItem.php
@@ -8,13 +8,13 @@ use InvalidArgumentException;
 class HeurekaItem extends BaseItem implements ItemInterface
 {
     protected $required = [
-		'ITEM_ID',
-		'PRODUCTNAME',
-		'DESCRIPTION',
-		'URL',
+        'ITEM_ID',
+        'PRODUCTNAME',
+        'DESCRIPTION',
+        'URL',
         'PRICE_VAT',
         'DELIVERY_DATE'
-	];
+    ];
 
     public static function fromProduct(Product $product)
     {
@@ -35,126 +35,126 @@ class HeurekaItem extends BaseItem implements ItemInterface
         });
     }
 
-	public function setId($id)
-	{
-		if (preg_match('/[^a-z_\-0-9]/i', $id)) {
-			throw new InvalidArgumentException("ID " . $id . " must be alphanumeric.");
-		}
+    public function setId($id)
+    {
+        if (preg_match('/[^a-z_\-0-9]/i', $id)) {
+            throw new InvalidArgumentException("ID " . $id . " must be alphanumeric.");
+        }
 
-		$this->row['ITEM_ID'] = $id;
-	}
+        $this->row['ITEM_ID'] = $id;
+    }
 
-	public function setName($name)
-	{
-		$this->row['PRODUCTNAME'] = '<![CDATA[' . $name . ']]>';
-	}
+    public function setName($name)
+    {
+        $this->row['PRODUCTNAME'] = $this->utf8_for_xml($name);
+    }
 
-	public function getName()
-	{
-		return $this->row['PRODUCTNAME'];
-	}
+    public function getName()
+    {
+        return $this->row['PRODUCTNAME'];
+    }
 
-	public function setUrl($url)
-	{
-		if(filter_var($url, FILTER_VALIDATE_URL) === false) {
-			throw new InvalidArgumentException('Is not valid URL: ' . $url);
-		}
+    public function setUrl($url)
+    {
+        if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+            throw new InvalidArgumentException('Is not valid URL: ' . $url);
+        }
 
-		$this->row['URL'] = $url;
-	}
+        $this->row['URL'] = $url;
+    }
 
-	public function getUrl()
-	{
-		return $this->row['URL'];
-	}
+    public function getUrl()
+    {
+        return $this->row['URL'];
+    }
 
-	public function setDescription($desc)
-	{
-		$this->row['DESCRIPTION'] = '<![CDATA[' . strip_tags($desc) . ']]>';
-	}
+    public function setDescription($desc)
+    {
+        $this->row['DESCRIPTION'] = $this->utf8_for_xml(strip_tags($desc));
+    }
 
-	public function setPrice($price)
-	{
-		$this->row['PRICE_VAT'] = round($price, 2);
-	}
+    public function setPrice($price)
+    {
+        $this->row['PRICE_VAT'] = round($price, 2);
+    }
 
-	public function setDeliveryDate($delivery)
-	{
-		$this->row['DELIVERY_DATE'] = $delivery;
-	}
+    public function setDeliveryDate($delivery)
+    {
+        $this->row['DELIVERY_DATE'] = $delivery;
+    }
 
-	// Recommended
+    // Recommended
 
-	public function setCategoryText($category)
-	{
-		$this->row['CATEGORYTEXT'] = $category;
-	}
+    public function setCategoryText($category)
+    {
+        $this->row['CATEGORYTEXT'] = $category;
+    }
 
-	public function setImage($image)
-	{
-		$this->row['IMGURL'] = $image;
-	}
+    public function setImage($image)
+    {
+        $this->row['IMGURL'] = $image;
+    }
 
-	public function addImageAlternate($image)
-	{
-		$this->row['IMGURL_ALTERNATE'][] = $image;
-	}
+    public function addImageAlternate($image)
+    {
+        $this->row['IMGURL_ALTERNATE'][] = $image;
+    }
 
-	public function setEan($ean)
-	{
-		$this->row['EAN'] = $ean;
-	}
+    public function setEan($ean)
+    {
+        $this->row['EAN'] = $ean;
+    }
 
-	public function setProductNo($no)
-	{
-		$this->row['PRODUCTNO'] = $no;
-	}
+    public function setProductNo($no)
+    {
+        $this->row['PRODUCTNO'] = $no;
+    }
 
-	public function setItemgroupId($group)
-	{
-		$this->row['ITEMGROUP_ID'] = $group;
-	}
+    public function setItemgroupId($group)
+    {
+        $this->row['ITEMGROUP_ID'] = $group;
+    }
 
-	public function setManufacturer($man)
-	{
-		$this->row['MANUFACTURER'] = $man;
-	}
+    public function setManufacturer($man)
+    {
+        $this->row['MANUFACTURER'] = $man;
+    }
 
-	public function setProduct($product)
-	{
-		$this->row['PRODUCT'] = $product;
-	}
+    public function setProduct($product)
+    {
+        $this->row['PRODUCT'] = $product;
+    }
 
-	public function setMaxCPC($cpc = NULL)
-	{
-		if ($cpc) {
-			if ($cpc < 1 || $cpc > 1000) {
-				throw new InvalidArgumentException('MAX CPC must be between 1 AND 1000 Kč');
-			} elseif (!is_numeric($cpc)) {
-				throw new InvalidArgumentException('MAX CPC must be numeric');
-			}
-			$this->row['HEUREKA_CPC'] = $cpc;
-		}
-	}
+    public function setMaxCPC($cpc = NULL)
+    {
+        if ($cpc) {
+            if ($cpc < 1 || $cpc > 1000) {
+                throw new InvalidArgumentException('MAX CPC must be between 1 AND 1000 Kč');
+            } elseif (!is_numeric($cpc)) {
+                throw new InvalidArgumentException('MAX CPC must be numeric');
+            }
+            $this->row['HEUREKA_CPC'] = $cpc;
+        }
+    }
 
-	public function addParam($name, $value, $unit = NULL)
-	{
-		 $param = [
-			"PARAM_NAME" => $name,
-			"VAL" => $value,
-		];
+    public function addParam($name, $value, $unit = NULL)
+    {
+        $param = [
+            "PARAM_NAME" => $name,
+            "VAL"        => $value,
+        ];
 
-		 if ($unit) {
-		     $param["UNIT"] = $unit;
-         }
+        if ($unit) {
+            $param["UNIT"] = $unit;
+        }
 
         $this->row["PARAM"][] = $param;
-	}
+    }
 
-	public function unsetParam()
-	{
-		unset($this->row['PARAM']);
-	}
+    public function unsetParam()
+    {
+        unset($this->row['PARAM']);
+    }
 
     /**
      * https://sluzby.heureka.cz/napoveda/xml-feed/#DELIVERY
@@ -163,25 +163,30 @@ class HeurekaItem extends BaseItem implements ItemInterface
      * @param null $price
      * @param null $price_cod
      */
-	public function addDelivery($name, $price = NULL, $price_cod = NULL)
-	{
-		$arr['DELIVERY_ID'] = $name;
+    public function addDelivery($name, $price = NULL, $price_cod = NULL)
+    {
+        $arr['DELIVERY_ID'] = $name;
 
-		if (!is_null($price)) {
-			$arr['DELIVERY_PRICE'] = $price;
-		}
+        if (!is_null($price)) {
+            $arr['DELIVERY_PRICE'] = $price;
+        }
 
-		if (!is_null($price_cod)) {
-			$arr['DELIVERY_PRICE_COD'] = $price_cod;
-		}
+        if (!is_null($price_cod)) {
+            $arr['DELIVERY_PRICE_COD'] = $price_cod;
+        }
 
-		$this->row['DELIVERY'][] = $arr;
-	}
+        $this->row['DELIVERY'][] = $arr;
+    }
 
-	public function setVideo($video)
-	{
-		if(stripos($video, 'youtube.com') !== false) {
-			$this->row['VIDEO_URL'] = $video;
-		}
-	}
+    public function setVideo($video)
+    {
+        if (stripos($video, 'youtube.com') !== false) {
+            $this->row['VIDEO_URL'] = $video;
+        }
+    }
+
+    public function utf8_for_xml($string)
+    {
+        return preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', ' ', $string);
+    }
 }
