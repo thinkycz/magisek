2c73a23f2f76a6711edd39e23fd3ad8a30384e69
Long Do <me@thinky.cz>
2020-11-01 18:20:38 +0100

    Metrics removed from admin and document layouts

--- resources/views/client/layout.blade.php
+++ resources/views/client/layout.blade.php
@@ -23,3 +23,40 @@
 
     @include('client.partials.footer')
 @endsection
+
+@section('metrics')
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRGNZDM"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                xfbml            : true,
+                version          : 'v8.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/cs_CZ/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <!-- Your Chat Plugin code -->
+    <div class="fb-customerchat"
+         attribution=setup_tool
+         page_id="112907143829486"
+         theme_color="#3F92A0"
+         logged_in_greeting="Jak ti může Magísek pomoci?"
+         logged_out_greeting="Jak ti může Magísek pomoci?">
+    </div>
+@endsection
