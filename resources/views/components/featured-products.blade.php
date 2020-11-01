6e560b10b43931a061abff943730117801211317
Long Do <me@thinky.cz>
2020-11-01 18:30:04 +0100

    Product slider is now clickable

--- resources/views/components/featured-products.blade.php
+++ resources/views/components/featured-products.blade.php
@@ -1,6 +1,6 @@
 <div class="flex-1 flex flex-col space-y-8">
     @forelse($categories as $category)
-        <x-product-slider :name="$category->name" :products="$category->products()->take(6)->get()"></x-product-slider>
+        <x-product-slider :name="$category->name" :products="$category->products()->inRandomOrder()->take(6)->get()" :link="route('categories.show', $category)"></x-product-slider>
     @empty
         <x-product-slider :name="__('global.newest_products')" :products="App\Models\Product::latest()->take('6')->get()"></x-product-slider>
     @endforelse
--- resources/views/components/product-slider.blade.php
+++ resources/views/components/product-slider.blade.php
@@ -1,8 +1,12 @@
-@props(['name', 'products'])
+@props(['name', 'products', 'link' => null])
 
 <div class="space-y-4 flex-1">
     <h2 class="text-2xl text-gray-700 font-semibold">
-        {{ $name }}
+        @if($link)
+            <a href="{{ $link }}" class="hover:underline">{{ $name }}</a>
+        @else
+            {{ $name }}
+        @endif
     </h2>
 
     <ul class="grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
