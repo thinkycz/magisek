7c7dca23598d2136210c60774fa99c8f4f5dba05
Long Do <me@thinky.cz>
2020-11-01 18:21:25 +0100

    Metrics removed from admin and document layouts

--- resources/views/client/layout.blade.php
+++ resources/views/client/layout.blade.php
@@ -25,13 +25,6 @@
 @endsection
 
 @section('metrics')
-    <!-- Google Tag Manager (noscript) -->
-    <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRGNZDM"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    </noscript>
-    <!-- End Google Tag Manager (noscript) -->
-
     <!-- Load Facebook SDK for JavaScript -->
     <div id="fb-root"></div>
     <script>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -51,6 +51,13 @@
     </head>
 
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRGNZDM"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         @yield('metrics')
 
         @yield('body')
